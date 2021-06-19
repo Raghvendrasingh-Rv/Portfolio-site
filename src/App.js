907869e9d731805354fbecc857090642c986cbfc
Raghvendra <rv.singh.5330@gmail.com>
2021-06-19 18:23:42 +0530

    completed website first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React from 'react'
+import GitHub from './MyComponent/GitHub'
+import Navbar from './MyComponent/Navbar';
+import {
+  BrowserRouter,
+  Route
+} from "react-router-dom"
+
+
 import './App.css';
+import Projects from './Projects';
+import Home from './Home';
+import Services from './Services';
+import Footer from './Footer';
+import About from './About'
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <BrowserRouter>
+        <Route exact path="/">
+          <Navbar />
+          <Home />
+          <About />
+          <Services />
+          <GitHub />
+          <Projects />
+          <Footer />
+        </Route>
+      </BrowserRouter>
+    </>
   );
 }
 
 export default App;
+
+
